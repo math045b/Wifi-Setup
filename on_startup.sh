1b58318b762320738ad3e7b8864d95c4e451769e
math045b <62756914+math045b@users.noreply.github.com>
2021-07-27 21:07:46 +0200

    Update on_startup.sh

--- on_startup.sh
+++ on_startup.sh
@@ -2,6 +2,10 @@
 
 # Ping google to check for internet connection
 if ! ping -c2 google.com; then 
-  echo " "
-  echo "No network connection"
+  echo ' '
+  echo 'No network connection'
+  flask run
+else
+  echo ' '
+  echo 'Connected'
 fi
