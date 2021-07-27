44bcbf4dc6369e2958c029a1d5274d28657f98a0
math045b <62756914+math045b@users.noreply.github.com>
2021-07-27 22:25:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-apt update
-sudo -u pi pip install Flask
+echo 'Setup Started'
+apt-get update
+
+echo 'Installing pip'
+apt-get install python3-pip -y
+
+echo 'Installing Flask'
+sudo -u pi pip3 install Flask
+
+echo 'Adding startup script to rc.local'
 path=$(pwd)
-echo 'bash ${path##*/on_startup.sh}' >> /etc/rc.local
-sudo -u pi bash ${path##*/on_startup.sh}
+echo "bash ${path}/on_startup.sh" >> /etc/rc.local
+echo 'Running startup script'
+sudo -u pi bash ${path}/on_startup.sh
